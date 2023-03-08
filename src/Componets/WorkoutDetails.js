4cc5fa061afa1c3aeed25ad526e464edc72f1795
Spencer Dresmann <sdresmann@thekablegroup.com>
2023-03-08 15:33:48 -0500

    update

--- src/Componets/WorkoutDetails.js
+++ src/Componets/WorkoutDetails.js
@@ -13,7 +13,7 @@ const WorkoutDetails = ({workout}) => {
         }
 
 
-        const response = await fetch('/api/workouts/' + workout._id, {
+        const response = await fetch('https://workoutbackend-api.onrender.com/api/workouts/' + workout._id, {
             method: 'DELETE',
             headers:{
                 'Authorization': `Bearer ${user.token}`
