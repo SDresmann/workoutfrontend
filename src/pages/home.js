25cfffb00e589e16d6e93af9c22ecdb69ad32962
Spencer Dresmann <sdresmann@thekablegroup.com>
2023-03-08 16:15:02 -0500

    go me

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
     useEffect(() => {
         const fetchWorkouts =  async () => {
-            const response = await fetch('/api/workouts', {
+            const response = await fetch('https://workoutbackend-api.onrender.com/api/workouts', {
                 headers: {
                     'Authorization': `Bearer ${user.token}`
                 }
